ecfe98595fab47ae426c7d4df4faa805db373179
yashyc7 <yashc9411407582@gmail.com>
2024-04-29 12:33:24 +0530

    .vercel.app allowed hosts

--- backup_portfolio3d/settings.py
+++ backup_portfolio3d/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+k7%94_4nc39a8cvd)nk+_k(hd81a4i(moo3dn$rzmz*fg#6j=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
